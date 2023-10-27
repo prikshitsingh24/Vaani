ba6203df5a8148dadd2d60e0a0df48c6a2a56fcb
Prikshit singh <prikshitsingh79@gmail.com>
2023-10-27 12:44:57 +0530

    Create wordPreprocessor function

--- Model/data/preprocessor.py
+++ Model/data/preprocessor.py
@@ -1,5 +1,6 @@
 from customData import CustomDataset
-word = input("enter alphabet or number:")  #priyanshu ALIML algorithm
-ascii_values = sum(ord(char) for char in word)
+def wordPreproccessor(word):
+    ascii_values = sum(ord(char) for char in word)
+    print(f"ASCII values for '{word}': {ascii_values}")
 
-print(f"ASCII values for '{word}': {ascii_values}")
+
