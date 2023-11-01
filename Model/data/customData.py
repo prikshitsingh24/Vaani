b65d215d085dac0ce3a395c9acd9b3c5c1b585ae
priyanshu <singhpriyanshu783@gmail.com>
2023-11-01 17:03:58 +0530

    Complete Gan model

--- Model/data/customData.py
+++ Model/data/customData.py
@@ -1,17 +1,23 @@
 import torch
 from torch.utils.data import Dataset
+from torchvision import datasets
+from data.preprocessor import wordPreproccessor
 
 class CustomDataset(Dataset):
-    def __init__(self, data, labels):
-        self.data = data
-        self.labels = labels
+    def __init__(self, data,labels, transform=None):
+        self.data = datasets.ImageFolder(root=data, transform=transform)
+        self.labels=labels
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        sample = {
-            'data': self.data[idx],
-            'label': self.labels[idx]
-        }
-        return sample
+        data, label = self.data[idx] 
+        
+        customLabel=label+1
+        # label = self.labels[idx]
+        print(customLabel)
+        return data,customLabel
+            
+       
+        
--- Model/data/preprocessor.py
+++ Model/data/preprocessor.py
@@ -1,6 +1,15 @@
-from customData import CustomDataset
-def wordPreproccessor(word):
-    ascii_values = sum(ord(char) for char in word)
-    print(f"ASCII values for '{word}': {ascii_values}")
 
-
+def wordPreproccessor(glosses):
+    
+    # ascii_values = sum(ord(char) for char in word)
+    # print(f"ASCII values for '{word}': {ascii_values}")
+    userInput=[]
+    baseValue=10
+    for i in glosses:
+        
+        if (ord(i) >=65 and ord(i) <91 ):
+            gen=ord(i)-65
+            userInput.append( int(gen+baseValue) ) 
+        else:
+             userInput.append(int(i)) 
+    return userInput
